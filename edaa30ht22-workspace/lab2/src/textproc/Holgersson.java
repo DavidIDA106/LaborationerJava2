6b2a567fa34f8cebe73748e35f500308e2380e48
David Petersson <da6133pe-s@student.lu.se>
2022-09-28 14:01:23 +0200

    nuså

--- edaa30ht22-workspace/lab2/src/textproc/Holgersson.java
+++ edaa30ht22-workspace/lab2/src/textproc/Holgersson.java
@@ -2,8 +2,6 @@ package textproc;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Holgersson {
